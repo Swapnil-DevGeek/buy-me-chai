f727f17a1c24363efd0d5db74783abbaa6bac54d
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-04-22 22:04:34 +0530

    set up next auth via github

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,17 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
+import { useSession,signIn,signOut } from "next-auth/react";
 
 const Navbar = () => {
+  const {data : session} = useSession();
+  if(session){
+    return<>
+      Signed in as {session.user.email} <br/>
+      <button onClick={()=>signOut()}>Log out</button>
+    </>
+  }
+  
   return (
     <nav className='bg-gray-900 text-white flex items-center justify-between h-16 px-4 '>
         
@@ -12,7 +22,7 @@ const Navbar = () => {
         </Link>
         
       <Link href='/login'>
-      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+      <button type="button"  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
         Login
       </button>
       </Link>
